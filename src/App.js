eeb948e88b86b8dc96383dca04fd9a4b850ebf52
Alberto Alejandro Larios Manzano <alexpgr.x4@gmail.com>
2020-03-23 11:39:48 -0600

    Fix circleci file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import Home from './Components/Home'; 
+import Home from './Components/Home/Home'; 
 import './App.css';
 
 function App() {
